1011b8421731c77f63b79b8ae1e4d1baa38011f2
Hongzhe Cheng <hongzhechen02@gmail.com>
2022-09-07 05:51:58 +0000

    index page

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -83,13 +83,11 @@ def registration_request(request):
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
     if request.method == "GET":
-        url = "https://us-east.functions.appdomain.cloud/api/v1/web/hongzhe-practice_hongzhe-practice-space/dealership/dealer-get"
-        # Get dealers from the URL
+        context = {}
+        url = "https://a3795162.eu-gb.apigw.appdomain.cloud/api2/getdealerships"
         dealerships = get_dealers_from_cf(url)
-        # Concat all dealer's short name
-        dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
-        # Return a list of dealer short name
-        return HttpResponse(dealer_names)
+        context["dealership_list"] = dealerships
+        return render(request, 'djangoapp/index.html', context)
 
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
